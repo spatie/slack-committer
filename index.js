0f3f1df24908ccccaf88246a40f17b5c3199b1dc
Ruben Van Assche <ruben@spatie.be>
2020-08-06 13:52:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const mapping = new Map([
+    ['rubenvanassche', 'UCK2H2Z6V'],
+    ['brendt', 'U7B46B49W'],
+    ['sebastiandedeyne', 'U04SG19KQ'],
+    ['AdrianMrn', 'UCKJ7ATPU'],
+    ['freekmurze', 'U0251B640'],
+    ['AlexVanderbist', 'U3Y68BH29'],
+    ['willemvb', 'U025151HP'],
+    ['riasvdv', 'UJA4K0423'],
+])
+
+try {
+    const committer = github.context.actor;
+
+    const output = mapping.has(committer)
+        ? `<@${mapping.get(committer)}>`
+        : committer;
+
+    core.setOutput("username", output);
+} catch (error) {
+    core.setFailed(error.message);
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Slack committer action
+
+This action transforms the committer of the current commit into a slack friendly @name so the Spatie team member will be mentioned. When a non-Spatie member commits, the action will use the committers GitHub username.
+
+## Example usage
+
+```yaml
+-   name: Resolve slack committer
+    id: slack-committer
+    uses: spatie/slack-committer@v1
+```
+
+Later in your workflow:
+
+```yml
+-   name: Slack Notification
+    uses: rtCamp/action-slack-notify@master
+    env:
+        SLACK_WEBHOOK: ${{ secrets.SLACK_WEBHOOK }}
+        SLACK_MESSAGE: "You should fix this: ${{ steps.slack-committer.outputs.username }}"
+``
