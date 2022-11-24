5a254cd62ba0cb502c02490d187f9f98c4cc2bba
Sebastian Loh <sebastian@cove.is>
2022-11-24 13:31:26 -0500

    test

--- index.js
+++ index.js
@@ -13,9 +13,16 @@ try {
 
     console.log('foo', githubToSlackId)
 
-    const output = githubToSlackId.has(committer)
-        ? `<${githubToSlackId.get(committer)}>`
-        : committer;
+    let slackRecipient = githubToSlackId.get(committer) || githubToSlackId.get('default');
+
+    if (!slackRecipient) {
+        throw new Error(`Could not determine the slack username for ${committer}. Verify the "user-mapping" is correct:\n${JSON.stringify(inputJSON, undefined, 2)}`);
+    }
+
+    if (!(slackRecipient.startsWith('#') || slackRecipient.startsWith('@'))) {
+        slackRecipient = `@${slackRecipient}`
+    }
+    const output = `<${slackRecipient}>`
 
     core.setOutput("username", output);
 } catch (error) {
