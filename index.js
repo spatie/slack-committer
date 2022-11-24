294a46da171e9503dad4920091116e13a37e3509
Sebastian Loh <sebastian@cove.is>
2022-11-23 19:11:16 -0500

    test

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ try {
     const inputJSON = JSON.stringify(core.getInput('user-mapping'));
     const githubToSlackId = new Map(Object.entries(inputJSON).map(([key, value]) => ([key.toLocaleLowerCase(), value])))
 
+    console.log('foo', githubToSlackId)
 
     const output = githubToSlackId.has(committer)
         ? `<${githubToSlackId.get(committer)}>`
