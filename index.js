45276e9cb48d4713d48da2f5f221d8075c9e8ed7
Sebastian Loh <sebastian@cove.is>
2022-11-24 13:35:00 -0500

    cleanup

--- index.js
+++ index.js
@@ -2,27 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 
-
-
 try {
     const committer = github.context.actor?.toLowerCase();
 
     const inputJSON = JSON.parse(core.getInput('user-mapping'));
-    console.log('bar', inputJSON)
     const githubToSlackId = new Map(Object.entries(inputJSON).map(([key, value]) => ([key.toLocaleLowerCase(), value])))
 
-    console.log('foo', githubToSlackId)
+    const output = githubToSlackId.get(committer) || githubToSlackId.get('fallback');
 
-    let slackRecipient = githubToSlackId.get(committer) || githubToSlackId.get('default');
-
-    if (!slackRecipient) {
+    if (!output) {
         throw new Error(`Could not determine the slack username for ${committer}. Verify the "user-mapping" is correct:\n${JSON.stringify(inputJSON, undefined, 2)}`);
     }
-
-    if (!(slackRecipient.startsWith('#') || slackRecipient.startsWith('@'))) {
-        slackRecipient = `@${slackRecipient}`
-    }
-    const output = `<${slackRecipient}>`
 
     core.setOutput("username", output);
 } catch (error) {
